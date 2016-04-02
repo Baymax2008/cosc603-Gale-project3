9927821ea616327e2f6b30f29347bbebb0892968
Baymax2008 <Matt.John.Gale@gmail.com>
2016-04-02 13:47:46 -0400

    Created Test cases for VendingMachineItemTest

--- VendingMachine/src/edu/towson/cis/cosc603/project4/vendingmachine/VendingMachineItemTest.java
+++ VendingMachine/src/edu/towson/cis/cosc603/project4/vendingmachine/VendingMachineItemTest.java
@@ -6,29 +6,47 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+//import edu.towson.cis.cosc603.project4.rectangle.Rectangle;
+
 public class VendingMachineItemTest {
+	
+	/** Declaring necessary test objects for  */
+	VendingMachineItem reeseCup;
 
 	@Before
 	public void setUp() throws Exception {
+		
+		reeseCup = new VendingMachineItem( "Reese Cup", 1.00);
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		
+		reeseCup = null;
+		
+	}
+
+	@Test(expected = VendingMachineException.class)
+	public final void testVendingMachineItemNegPrice() {
+		VendingMachineItem butterFingers = new VendingMachineItem( "Butter Fingers", -1.00);
 	}
 
 	@Test
-	public final void testVendingMachineItem() {
-		fail("Not yet implemented"); // TODO
+	public final void testVendingMachineItemNormal() {
+		String butterFingerName = "Butter Fingers";
+		VendingMachineItem butterFingers = new VendingMachineItem( butterFingerName, 1.00);
+		assertEquals(butterFingerName, butterFingers.getName());
 	}
-
+	
 	@Test
 	public final void testGetName() {
-		fail("Not yet implemented"); // TODO
+		assertEquals("Reese Cup", reeseCup.getName());
 	}
 
 	@Test
 	public final void testGetPrice() {
-		fail("Not yet implemented"); // TODO
+		assertEquals(1.00, reeseCup.getPrice(),0.001);
 	}
 
 }
