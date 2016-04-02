438a172620b616dbb1e03fc66130aef520fec265
Baymax2008 <Matt.John.Gale@gmail.com>
2016-04-02 13:15:34 -0400

    Created the JUnit source files.

---
+++ VendingMachine/src/edu/towson/cis/cosc603/project4/vendingmachine/VendingMachineItemTest.java
@@ -0,0 +1,34 @@
+package edu.towson.cis.cosc603.project4.vendingmachine;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VendingMachineItemTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testVendingMachineItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetPrice() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ VendingMachine/src/edu/towson/cis/cosc603/project4/vendingmachine/VendingMachineTest.java
@@ -0,0 +1,49 @@
+package edu.towson.cis.cosc603.project4.vendingmachine;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VendingMachineTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testAddItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testRemoveItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testInsertMoney() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetBalance() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testMakePurchase() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testReturnChange() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
